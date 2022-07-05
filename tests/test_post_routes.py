e113f50b04ccb73488ad378fd80f92c5362c4b6f
“Lamaosman2012” <lamaosman@yahoo.de>
2022-07-05 12:56:27 +0200

    fixed some errors

--- tests/test_post_routes.py
+++ tests/test_post_routes.py
@@ -11,7 +11,6 @@ from class_social.main import app
 def http_test_client():
     return TestClient(app)
 
-
 valid_post_list = [{
     'title': 'Some_title',
     'body': 'text_content',
@@ -23,71 +22,23 @@ valid_post_list = [{
     'created_on': "2023-03-27T00:00:00.000+00:00",
     'is_active': True,
     'address': 'some_address',
-    'role': 'Student'
-    },
-    'creation_date': datetime.datetime.now(),
-    'type_of_post': 'Text'
-},
-{
-    'title': 'Some_title_2',
-    'body': 'image_content',
-    'creator': {'id': 'c2',
-    'name': 'Mat',
-    'username': 'mat',
-    'password': 'some_pass',
-    'email': 'mathias@mat',
-    'created_on': "2023-03-27T00:00:00.000+00:00",
-    'is_active': True,
-    'address': 'some_address',
-    'role': 'Student'
-    },
-    'creation_date': datetime.datetime.now(),
-    'type_of_post': 'Image'
-},
-{
-    'title': 'Some_title_2',
-    'body': 'image_content',
-    'creator': {'id': 'c3',
-    'name': 'Mat',
-    'username': 'mat',
-    'password': 'some_pass',
-    'email': 'mathias@mat',
-    'created_on': "2023-03-27T00:00:00.000+00:00",
-    'is_active': True,
-    'address': 'some_address',
     'role': 'Teacher'
     },
     'creation_date': datetime.datetime.now(),
-    'type_of_post': 'Image'
-},
-{
-    'title': 'Some_title_2',
-    'body': 'image_content',
-    'creator': {'id': 'c4',
-    'name': 'Mat',
-    'username': 'mat',
-    'password': 'some_pass',
-    'email': 'mathias@mat',
-    'created_on': "2023-03-27T00:00:00.000+00:00",
-    'is_active': True,
-    'address': 'some_address',
-    'role': 'Institution'
-    },
-    'creation_date': datetime.datetime.now(),
-    'type_of_post': 'Image'
-}
-]
+    'type_of_post': 'Text'
+}]
 
 
-def test_given_an_id_for_an_existent_user_system_must_return_200_ok(http_test_client):
+def test_given_an_existent_creator_role_for_the_user_system_must_return_post_list_and_200_ok(http_test_client):
     with patch('class_social.posts.post_controller') as controller_mock:
         controller_mock.get_posts_by_creator_role = Mock(return_value=valid_post_list)
-        response = http_test_client.get('/posts/creator/{role}')
+        response = http_test_client.get('posts/creator/{role}')
+        print(response.text)
         assert response.status_code == 200
 
 
 def test_given_a_nonexistent_creator_role_system_must_return_404_ok(http_test_client):
-    with patch('class_social.users.user_controller') as controller_mock:
+    with patch('class_social.posts.post_controller') as controller_mock:
         controller_mock.get_posts_by_creator_role = Mock(return_value=None)
-        response = http_test_client.get('/posts/creator/nonexistent_role')
+        response = http_test_client.get('/posts/creator/anything')
         assert response.status_code == 404
