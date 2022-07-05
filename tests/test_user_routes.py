08650c41bba3246a54901e929b477d1362800f25
petertheprofessional <peterkelly3398@gmail.com>
2022-07-05 13:08:34 +0200

    thrid commit

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -24,7 +24,7 @@ valid_user = {
 }
 
 valid_obj_user = User(id='someid', name='Mathias', username='mathias', password='somepass', email='mathias@mathias',
-         created_on="2023-03-27T00:00:00.000+00:00", is_active=True, address="some_address", role="Teacher")
+          created_on="2023-03-27T00:00:00.000+00:00", is_active=True, address="some_address", role="Teacher")
 
 
 def test_given_valid_new_user_data_the_system_must_register_the_user_and_return_200_ok(http_test_client):
@@ -55,7 +55,7 @@ def test_if_new_user_email_already_exists_in_system_and_return_404_if_used(http_
 
 def test_given_an_id_for_an_existent_user_system_must_return_200_ok(http_test_client):
     with patch('class_social.users.user_controller') as controller_mock:
-        controller_mock.get_user_by_id = Mock(return_value=valid_user)
+        controller_mock.get_user_by_id = Mock(return_value=valid_obj_user)
         response = http_test_client.get('/users/c1')
         assert response.status_code == 200
 
@@ -77,7 +77,7 @@ def test_given_is_active_is_true_for_an_existent_user_system_must_return_200_ok(
 def test_given_is_active_is_false_for_an_existent_user_system_must_return_404_ok(http_test_client):
     with patch('class_social.users.user_controller') as controller_mock:
         controller_mock.get_user_by_is_active = Mock(return_value=valid_obj_user)
-        response = http_test_client.get('/users/c1/is_active')
+        response = http_test_client.get('/users/someid/is_active')
         assert response.status_code == 404
 
 
@@ -87,7 +87,7 @@ def test_must_always_return_a_list_of_users_and_200_ok(http_test_client):
         response = http_test_client.get('/users')
         assert response.status_code == 200
 
-        controller_mock.get_users = Mock(return_value=[valid_user])
+        controller_mock.get_users = Mock(return_value=[valid_obj_user])
         response = http_test_client.get('/users')
         assert response.status_code == 200
 
