20c705e1838c43c79d60326e141f0fbad12afb4e
Mathias Santos de Brito <mathias.brito@me.com>
2022-07-07 14:22:27 +0200

    pull request #30 review

--- class_social/db.py
+++ class_social/db.py
@@ -1,11 +1,9 @@
 import pickle
-import pymongo
+
 
 class DBException(Exception):
     pass
 
-client = pymongo.MongoClient("localhost", 27018)
-db = client.class_social
 
 def save_users(users):
     with open('users.pickle', 'wb') as file:
--- class_social/file_importing.py
+++ class_social/file_importing.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, File, UploadFile
 from typing import List
-from deta import Drive
+from deta import Deta
+import os
 
 upload_router = APIRouter()
 
-files__ = Drive("myfiles")
-
+token = os.environ.get("DETA_PROJECT_KEY")
+deta = Deta(token)
+files__ = deta.Drive("myfiles")
 
 @upload_router.post('/teacher/upload', tags=["files"])
 async def upload(files: List[UploadFile] = File(...)):
