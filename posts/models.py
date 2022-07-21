4af06e68ecb0e2ea0652365585b52add8568597c
“Lamaosman2012” <lamaosman@yahoo.de>
2022-07-21 20:34:53 +0200

    typos fixed

--- posts/models.py
+++ posts/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # from djangotoolbox.fields import ListField
 
 """Some times when you try to alter some column field in the data base it causes some problem and pycharm ask you to make nullable by setting a default value.  
-This could be solved by deleting all the migration (klick on the migrations package in the project navigator to delete them
-then delete the db.sqlite3 file then reinitiate the migration"""
+This could be solved by deleting all the migration (click on the migrations package in the project navigator to delete them
+then delete the db.sqlite3 file then initiate the migration again"""
 
 
 # todo initial simple user model just to test the post models
