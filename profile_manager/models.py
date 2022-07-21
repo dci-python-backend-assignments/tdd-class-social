f682211f56e5d5900d146912b8218d6e6bad8fea
Franz Bandelin <bifiroll86@gmail.com>
2022-07-21 15:37:53 +0200

    super awesome models. done. until now.

---
+++ profile_manager/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+
+# Create your models here.
+
+# QUESTION: how to filter for different fields of courses? Like current, completed, active, old
+# QUESTION: two models in many to many field, like Union[students, teachers]
+# QUESTION: List of phone numbers, how to JSONFIELD
+# QUESTION country codes for phone numbers
+
+class BaseUser(models.Model):
+    id = models.AutoField(primary_key=True)
+    password = models.CharField(max_length=200)
+    email = models.EmailField()
+    created_on = models.DateTimeField(auto_now_add=True, blank=True)
+    is_active = models.BooleanField(default=False)
+    address = models.CharField(max_length=300, null=True, blank=True)
+    website = models.URLField(max_length=300, null=True, blank=True)
+    about = models.TextField(max_length=1000, null=True, blank=True)
+    connections = models.ManyToManyField('self', symmetrical=False, related_name='users', null=True, blank=True)
+    ROLE_CHOICES = [('STD', 'Student'),
+                    ('INS', 'Institution'),
+                    ('TEC', 'Teacher'),
+                    ('OTH', 'Others')]
+    role = models.CharField(max_length=3, choices=ROLE_CHOICES, default='Others')
+
+
+# class Person(BaseUser):
+#
+#     first_name = models.CharField(max_length=150)
+#     last_name = models.CharField(max_length=150)
+#     date_of_birth = models.DateField()
+#     GENDER_CHOICES = [('U', 'Unicorn'),
+#                       ('F', 'Female'),
+#                       ('M', 'Male'),
+#                       ('O', 'Other')]
+#     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='Unicorn')
+#     # country codes???
+#     phone_number = models.IntegerField(max_length=30)
+#
+#
+# class Course(models.Model):
+#     pass
+#
+#
+# class ResearchProject(models.Model):
+#     pass
+#
+#
+# class Institution(BaseUser):
+#     name = models.CharField(max_length=300)
+#     phone_numbers = models.IntegerField(max_length=30)
+#     associates = models.ManyToManyField(BaseUser, symmetrical=False, related_name='institution', null=True, blank=True)
+#     association_requests = models.ManyToManyField(BaseUser, symmetrical=False, related_name='institution', null=True, blank=True)
+#     head_of_organization = models.CharField(max_length=300)
+#     research_institution = models.BooleanField(default=False, null=True, blank=True)
+#     education_institution = models.BooleanField(default=False, null=True, blank=True)
+#     research_projects = models.ManyToManyField(ResearchProject, symmetrical=False, related_name='institution', null=True, blank=True)
+#     active_courses = models.ManyToManyField(Course, symmetrical=False, related_name='institution', null=True, blank=True)
+#     inactive_courses = models.ManyToManyField(Course, symmetrical=False, related_name='institution', null=True, blank=True)
+#     offers = models.ManyToManyField(Course, symmetrical=False, related_name='institution', null=True, blank=True)
+#
+#
+# class Student(Person):
+#     current_courses = models.ManyToManyField(Course, symmetrical=False, related_name='students', null=True, blank=True)
+#     completed_courses = models.ManyToManyField(Course, symmetrical=False, related_name='students', null=True, blank=True)
+#     institution = models.ManyToManyField(Institution, symmetrical=False, related_name='students', null=True, blank=True)
+#     interests = models.CharField(max_length=200, null=True, blank=True)
+#
+#
+# class Teacher(Person):
+#     courses_in_progress = models.ManyToManyField(Course, symmetrical=False, related_name='teacher', null=True, blank=True)
+#     old_courses = models.ManyToManyField(Course, symmetrical=False, related_name='teacher', null=True, blank=True)
+#     future_courses = models.ManyToManyField(Course, symmetrical=False, related_name='teacher', null=True, blank=True)
+#     institution = models.ManyToManyField(Institution, symmetrical=False, related_name='teacher', null=True, blank=True)
+#     interests = models.CharField(max_length=200, null=True, blank=True)
