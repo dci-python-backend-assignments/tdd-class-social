703628e645669cc809f53118229dada3d6596cb0
petertheprofessional <peterkelly3398@gmail.com>
2022-08-18 14:23:02 +0200

    test2

--- social/serializers.py
+++ social/serializers.py
@@ -7,7 +7,6 @@ class BaseUserSerializer(HyperlinkedModelSerializer):
 
     class Meta:
         model = BaseUser
-        fields = ['id', 'username', 'password', 'email', 'is_active', 'address',
+        fields = ['id', 'username', 'password', 'email', 'created_on', 'is_active', 'address',
                   'website', 'about', 'connections', 'role', 'url']
 
-        # 'created_on'
