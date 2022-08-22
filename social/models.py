93d5dc66436189f838ef47faf3f730d45994d7d7
petertheprofessional <peterkelly3398@gmail.com>
2022-08-22 09:37:27 +0200

    model test commit

--- social/models.py
+++ social/models.py
@@ -7,15 +7,19 @@ class BaseUser(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
-    created_on = models.DateTimeField(auto_now_add=True, blank=True)
+    created_on = models.DateField(null=True, blank=True)
     is_active = models.BooleanField(default=False)
     address = models.CharField(max_length=300, null=True, blank=True)
     website = models.URLField(max_length=300, null=True, blank=True)
     about = models.TextField(max_length=1000, null=True, blank=True)
-    connections = models.ManyToManyField('self', symmetrical=False, related_name='users', blank=True)
+    #connections = models.ManyToManyField('self', symmetrical=False, related_name='users', blank=True)
+    connections = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
     ROLE_CHOICES = [('STD', 'Student'),
                     ('INS', 'Institution'),
                     ('TEC', 'Teacher'),
                     ('OTH', 'Others')]
     role = models.CharField(max_length=3, choices=ROLE_CHOICES, default='Others')
+
+    def __str__(self):
+        return self.username
 
--- social/tests.py
+++ social/tests.py
@@ -1,42 +1,25 @@
-# from django.test import TestCase
-# from datetime import datetime
-# from social.models import BaseUser
-# from unittest import mock
-#
-# class BaseUserModelTestCase(TestCase):
-#
-#     def test_creating_a_new_user_200(self):
-#         # mock_date = datetime(2022, 8, 18, 12, 10, 10, 703055)
-#         # with mock.patch('django.utils.timezone.now') as mock_now:
-#         #     mock_now.return_value = mock_date
-#         user = BaseUser.objects.create(username="peter", password="12345", email="peter@email.com", role="STD",
-#                                        address="Dresden", )
-#
-#         self.assertEqual(user.id, 1)
-#         self.assertEqual(user.username, "peter")
-#         self.assertEqual(user.password, "12345")
-#         self.assertEqual(user.email, "peter@email.com")
-#         # self.assertEqual(user.created_on, mock_date) # ask how to test this
-#         self.assertEqual(user.is_active, False)
-#         self.assertEqual(user.address, None)
-#         self.assertEqual(user.website, None)
-#         self.assertEqual(user.about, None)
-#         # self.assertEqual(user.connections, None) # ask how to test this
-#
-#     def test_new_user_has_duplicate_email_fail(self):
-#         # mock_date = datetime(2022, 8, 18, 12, 10, 10, 703055)
-#         # with mock.patch('django.utils.timezone.now') as mock_now:
-#         #     mock_now.return_value = mock_date
-#         user1 = BaseUser.objects.create(username="peter", password="12345", email="peter@email.com", role="STD")
-#         user2 = BaseUser.objects.create(username="kelly", password="54321", email="peter@email.com", role="STD")
-#
-#         self.assertEqual(user2.id, 2)
-#         self.assertEqual(user2.username, "kelly")
-#         self.assertEqual(user2.password, "54321")
-#         self.assertEqual(user2.email, "peter@email.com")
-#         # self.assertEqual(user.created_on, mock_date) # ask how to test this
-#         self.assertEqual(user2.is_active, False)
-#         self.assertEqual(user2.address, None)
-#         self.assertEqual(user2.website, None)
-#         self.assertEqual(user2.about, None)
-#         # self.assertEqual(user.connections, None) # ask how to test this
+from django.test import TestCase
+from datetime import datetime
+from social.models import BaseUser
+from unittest import mock
+
+class BaseUserModelTestCase(TestCase):
+
+    def test_creating_a_new_user_200(self):
+        user2 = BaseUser.objects.create(username="Kelly", password="12345", email="kelly@email.com", role="STD",
+                                        address="Dresden", created_on="2022-08-19")
+        user = BaseUser.objects.create(username="peter", password="12345", email="peter@email.com", role="STD",
+                                       address="Dresden", created_on="2022-08-19", connections=user2)
+
+        self.assertEqual(user.id, 2)
+        self.assertEqual(user.username, "peter")
+        self.assertEqual(user.password, "12345")
+        self.assertEqual(user.email, "peter@email.com")
+        self.assertEqual(user.created_on, "2022-08-19")
+        self.assertEqual(user.is_active, False)
+        self.assertEqual(user.address, "Dresden")
+        self.assertEqual(user.website, None)
+        self.assertEqual(user.about, None)
+        self.assertEqual(user.connections, user2)
+        self.assertEqual(user.role, "STD")
+
