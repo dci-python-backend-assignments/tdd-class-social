b3b2281b6de59cb6b7fa7b5aa426eb268138404a
Mathias Santos de Brito <mathias.brito@me.com>
2022-08-22 14:03:48 +0200

    clening up unecessary code

--- social/serializers.py
+++ social/serializers.py
@@ -1,12 +1,1 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
-
-from social.models import BaseUser
-
-
-class BaseUserSerializer(HyperlinkedModelSerializer):
-
-    class Meta:
-        model = BaseUser
-        fields = ['id', 'username', 'password', 'email', 'created_on', 'is_active', 'address',
-                  'website', 'about', 'connections', 'role', 'url']
 
