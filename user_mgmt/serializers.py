36fcfc60132d5a830a9c88e23e9590c2911fdd62
Hila <hilabit@gmail.com>
2022-08-23 15:09:48 +0200

    added serializers for institutions and student

--- user_mgmt/serializers.py
+++ user_mgmt/serializers.py
@@ -1,11 +1,30 @@
+from rest_framework.relations import HyperlinkedRelatedField
 from rest_framework.serializers import HyperlinkedModelSerializer
 
-from user_mgmt.models import BaseUser
+from user_mgmt.models import BaseUser, Institution, Student
 
 
 class BaseUserSerializer(HyperlinkedModelSerializer):
-
     class Meta:
         model = BaseUser
         fields = ['id', 'username', 'password', 'email', 'created_on', 'is_active', 'address',
                   'website', 'about', 'connections', 'url']
+
+
+class InstitutionSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Institution
+        fields = ['url', 'name', 'associates', 'head_of_organization', 'research_institution',
+                  'education_institution', 'courses']
+
+
+class StudentSerializer(HyperlinkedModelSerializer):
+    institution = HyperlinkedRelatedField(
+        queryset=Institution.objects.all(), many=True,
+        view_name='institution-detail')
+
+    class Meta:
+        model = Student
+        fields = ['url', 'first_name', 'last_name', 'courses', 'institution', 'interests', 'date_of_birth', 'gender']
+
--- user_mgmt/views.py
+++ user_mgmt/views.py
@@ -1,9 +1,33 @@
 from rest_framework.viewsets import ModelViewSet
 
-from user_mgmt.models import BaseUser
-from user_mgmt.serializers import BaseUserSerializer
+from user_mgmt.models import BaseUser, Student, Institution
+from user_mgmt.serializers import BaseUserSerializer, StudentSerializer, InstitutionSerializer
 
 
 class BaseUserViewSet(ModelViewSet):
     queryset = BaseUser.objects.all()
     serializer_class = BaseUserSerializer
+
+class StudentViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class InstitutionViewSet(ModelViewSet):
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+    def get_queryset(self):
+        def get_queryset(self):
+            search_term = self.request.query_params.get('search_terms')
+            if search_term:
+                queryset = Book.objects.filter(title__contains=search_term)
+            else:
+                queryset = Book.objects.all()
+
+            return queryset
+
+
+
+
+
