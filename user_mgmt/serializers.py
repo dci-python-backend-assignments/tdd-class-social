dff8d304639e372ad1e835e0bbf26534f64ddca0
Franz Bandelin <bifiroll86@gmail.com>
2022-08-23 16:21:19 +0200

    basic registration for institution added; need improvement

--- user_mgmt/serializers.py
+++ user_mgmt/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 
-from user_mgmt.models import BaseUser
+from user_mgmt.models import BaseUser, Institution, Student, Teacher
 
 
 class BaseUserSerializer(HyperlinkedModelSerializer):
@@ -9,3 +9,29 @@ class BaseUserSerializer(HyperlinkedModelSerializer):
         model = BaseUser
         fields = ['id', 'username', 'password', 'email', 'created_on', 'is_active', 'address',
                   'website', 'about', 'connections', 'url']
+
+class InstitutionSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Institution
+        fields = ['username', 'password', 'email']
+
+    def create(self, validated_data):
+        institution = Institution(**validated_data)
+        institution.save()
+        return institution
+
+class StudentSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+
+class TeacherSerializer(HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+
--- user_mgmt/views.py
+++ user_mgmt/views.py
@@ -1,9 +1,30 @@
+from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 
-from user_mgmt.models import BaseUser
-from user_mgmt.serializers import BaseUserSerializer
+from user_mgmt.models import BaseUser, Institution, Student, Teacher
+from user_mgmt.serializers import BaseUserSerializer, InstitutionSerializer, StudentSerializer, TeacherSerializer
 
 
 class BaseUserViewSet(ModelViewSet):
     queryset = BaseUser.objects.all()
     serializer_class = BaseUserSerializer
+
+
+class InstitutionViewSet(ModelViewSet):
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+
+class RegisterInstitution(generics.CreateAPIView):
+    queryset = Institution.objects.all()
+    serializer_class = InstitutionSerializer
+
+
+class StudentViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class TeacherViewSet(ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
