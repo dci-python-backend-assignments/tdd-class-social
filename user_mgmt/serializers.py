15bd4747ed9ebf4491635cc9061a884cd2b4fb19
petertheprofessional <peterkelly3398@gmail.com>
2022-08-24 15:07:16 +0200

    corrected serializer and added Pillow to the requirements

--- user_mgmt/serializers.py
+++ user_mgmt/serializers.py
@@ -8,4 +8,4 @@ class BaseUserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = BaseUser
         fields = ['id', 'username', 'password', 'email', 'created_on', 'is_active', 'address',
-                  'website', 'about', 'connections', 'url']
+                  'phone_number', 'avatar', 'website', 'about', 'connections', 'url']
