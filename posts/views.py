ad41b95225c1dc307d5ecd8fd501c4697af0cc0a
“Lamaosman2012” <lamaosman@yahoo.de>
2022-08-25 16:30:01 +0200

    Fix posts.serializers.py and add filtering by role methods to the PostViewSet
    To check if it works open the links:
     http://localhost:8000/posts/?posts_by=students
     http://localhost:8000/posts/?posts_by=teachers
     http://localhost:8000/posts/?posts_by=institutions

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from posts.models import Post, Comment
 from rest_framework.viewsets import ModelViewSet
 from posts.serializers import CommentSerializer, PostSerializer
+from user_mgmt.models import *
 
 
 class PostViewSet(ModelViewSet):
@@ -8,12 +9,27 @@ class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
 
     def get_queryset(self):
-        search_term = self.request.query_params.get('search_terms')
-        if search_term:
-            queryset = Post.objects.filter(creotor__contains=search_term)
-        else:
-            queryset = Post.objects.all()
-        return queryset
+        queryset = Post.objects.all()
+        search_key = list(self.request.query_params.keys())
+        if not search_key:
+            return queryset
+        elif search_key[0] == 'posts_by':
+            search_term = self.request.query_params.get(search_key[0])
+
+            if search_term == "students":
+                Student_ids = Student.objects.values_list('baseuser_ptr_id')
+                queryset = Post.objects.filter(creator__id__in=Student_ids)
+            elif search_term == "teachers":
+                Teacher_ids = Teacher.objects.values_list('baseuser_ptr_id')
+                queryset = Post.objects.filter(creator__id__in=Teacher_ids)
+            elif search_term == "institutions":  #
+                Institution_ids = Institution.objects.values_list('baseuser_ptr_id')
+                queryset = Post.objects.filter(creator__id__in=Institution_ids)
+
+            else:
+                queryset = []  # Post.objects.all()
+
+            return queryset
 
 
 class CommentViewSet(ModelViewSet):
